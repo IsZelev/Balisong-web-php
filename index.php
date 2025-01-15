06b0cbbda060d80e5c1051cd73caf93e15ae43db
Isaac <lorenzociani1002@gmail.com>
2025-01-15 13:32:31 +0100

    modifiche al php

--- index.php
+++ index.php
@@ -1,19 +1,17 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Ottieni i dati del form
-    $search = htmlspecialchars($_POST['search']);
-}
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+{
     $exp = "/([A-Z])\w+@([A-Z])\w+.([A-Z])+\w/gi";
     $email = htmlspecialchars($_POST['email']);
     
     if(preg_match($email, $exp) == 1)
     {
-        //va avanti alla homepage
+        //va avanti alla homepage  
+        return "Pages/Balisong_Web.html";
     }else
     {
         //messaggio di errore nella text box
+        echo '<script>alert("inserire un\'email valida")</script>';
     }
     
 }
